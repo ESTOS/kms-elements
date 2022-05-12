47116211f5b21e50656f53af7e66dfc27f51d1ad
Rolf Burkhardt <rolf.burkhardt@estos.de>
2022-05-12 09:44:04 +0200

    PROCALL-2090 add G722 - add support for G722

--- src/server/implementation/objects/WebRtcEndpointImpl.cpp
+++ src/server/implementation/objects/WebRtcEndpointImpl.cpp
@@ -62,7 +62,7 @@ static const guint DEFAULT_STUN_PORT = 3478;
 
 std::once_flag check_openh264, certificates_flag;
 std::string defaultCertificateRSA, defaultCertificateECDSA;
-std::vector<std::string> supported_codecs = { "VP8", "opus", "PCMU", "PCMA", "telephone-event"};
+std::vector<std::string> supported_codecs = { "VP8", "opus", "PCMU", "PCMA", "G722", "G729", "G726", "telephone-event"};
 
 static void
 remove_not_supported_codecs_from_array (GstElement *element, GArray *codecs)
